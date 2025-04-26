2d41739c169cc8398b9469c83d560eef260f33ba
Anatolii Sobchuk <89628705+nolikest@users.noreply.github.com>
2025-04-26 09:02:58 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'aphorism-cache-v1';
+const CACHE_NAME = 'aphorism-cache-v2';
 const urlsToCache = [
   './',
   './index.html',
@@ -11,6 +11,14 @@ const urlsToCache = [
 self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+    )
   );
 });
 
